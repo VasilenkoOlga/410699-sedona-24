46d6760212220cb73fc9f936c0156860b8b9f729
VasilenkoOlga <ovasilenko1207@gmail.com>
2022-01-19 22:57:48 +0300

    Начальные правки

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,14 @@ import plumber from 'gulp-plumber';
 import less from 'gulp-less';
 import postcss from 'gulp-postcss';
 import autoprefixer from 'autoprefixer';
+import csso from 'postcss-csso';
+import rename from 'gulp-rename';
+import htmlmin from 'gulp-htmlmin';
+import terser from 'gulp-terser';
+import squoosh from 'gulp-libsquoosh';
+import svgo from 'gulp-svgmin';
+import svgstore from 'gulp-svgstore';
+import del from 'del';
 import browser from 'browser-sync';
 
 // Styles
