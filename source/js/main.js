79eb6848972597fc82732d80186d25c102a2a3c0
VasilenkoOlga <ovasilenko1207@gmail.com>
2022-01-31 16:24:00 +0300

    Правки js и немного разметки

--- source/js/main.js
+++ source/js/main.js
@@ -4,6 +4,7 @@ var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.toggle-button');
 
 navMain.classList.remove('main-nav--nojs');
+navToggle.classList.remove('toggle-button--nojs');
 
 navToggle.addEventListener('click', function() {
   if (navMain.classList.contains('main-nav--close')) {
